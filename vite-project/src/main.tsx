f08e937d41c9d3a0ab64153b2b88c4e794c3b506
Lady786 <isroilovarobiyaxon786@gmail.com>
2024-07-19 18:35:59 +0100

    feat: main.tsx

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import { Provider } from 'react-redux';
-import { store } from './pages/reducer/store/store';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import {store} from './pages/reducer/store/store.ts'
 
-// O'zgarishlar
-const rootElement = document.getElementById('root') as HTMLElement;
-
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </React.StrictMode>
-  );
-}
+    </BrowserRouter>
+  </React.StrictMode>,
+)
